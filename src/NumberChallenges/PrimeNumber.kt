8b8e6c8cb81ed711013f5d17ad5061ae16c526a0
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-03 06:26:45 +0700

    Add function Prime Number

---
+++ src/NumberChallenges/PrimeNumber.kt
@@ -0,0 +1,15 @@
+package NumberChallenges
+
+fun main(args: Array<String>) {
+    for (i in 2..100) {
+        var isPrime: Boolean = true
+        for (j in 1..i) {
+            if (i % j === 0 && j !== 1 && j !== i) {
+                isPrime = false
+                break
+            }
+        }
+        if (isPrime)
+            println(i)
+    }
+}
