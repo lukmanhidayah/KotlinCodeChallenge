c044719329bb6fc9ad64a12d76ba70fd3051440b
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-03 06:31:22 +0700

    Add odd number function

---
+++ src/NumberChallenges/OddNumber.kt
@@ -0,0 +1,10 @@
+package NumberChallenges
+
+fun main(args: Array<String>) {
+    // How to print odd number in kotlin
+    for (i in 1..100) {
+        if (i % 2 === 0) {
+            println(i)
+        }
+    }
+}
