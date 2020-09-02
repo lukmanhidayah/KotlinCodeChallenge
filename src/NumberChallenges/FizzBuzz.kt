1df248c13b9e3d4c8a902261208339963bed397c
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-03 06:49:18 +0700

    Add how to solve fizzbuzz problem

---
+++ src/NumberChallenges/FizzBuzz.kt
@@ -0,0 +1,26 @@
+package NumberChallenges
+
+fun main(args: Array<String>) {
+    /*
+    * Write a program that prints the numbers from 1 to 100.
+    * But for multiples of three print “Fizz”
+    * instead of the number and for the multiples of five print “Buzz”.
+    * For numbers which are multiples of both three and five print “FizzBuzz"
+    */
+    var multi3 = 3
+    var multi5 = 5
+    for (i in 1..100) {
+        if (i === multi3 && i === multi5) {
+            println("FizzBuzz")
+            multi3 += 3
+            multi5 += 5
+        } else if (i === multi3) {
+            println("Fizz")
+            multi3 += 3
+        } else if (i === multi5) {
+            println("Buzz")
+            multi5 += 5
+        }
+
+    }
+}
