18a527798d21b7a8e0d100e6503e5db63ecd7fd1
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-03 06:30:30 +0700

    Add comment

--- src/NumberChallenges/PrimeNumber.kt
+++ src/NumberChallenges/PrimeNumber.kt
@@ -1,6 +1,7 @@
 package NumberChallenges
 
 fun main(args: Array<String>) {
+    // How to print prime number in kotlin
     for (i in 2..100) {
         var isPrime: Boolean = true
         for (j in 1..i) {
