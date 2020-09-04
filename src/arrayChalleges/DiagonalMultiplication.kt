b7983575d24f1d69ebd76d67329a06f194d12c0b
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-05 00:25:38 +0700

    Create diagonal matrix sum challenge

---
+++ src/arrayChalleges/DiagonalMultiplication.kt
@@ -0,0 +1,47 @@
+package arrayChalleges
+
+// Create array of integer contain two row and two column
+// multiplication diagonal of array and sum every result multiplication
+// example:
+// [1, 2, 3]
+// [1, 2, 3]
+// [1, 2, 3]
+// (1 * 4) + (2 * 3) = ....
+
+fun multiDiagonal(arr: List<List<Int>>) {
+    println("range array ${arr.indices}")
+    var diagonal1: Int = 1;
+    var diagonal2: Int = 1
+    for (i in arr.indices) {
+        for (j in arr[i].indices) {
+            if (i == j && j + i == arr.size - 1) {
+                diagonal1 *= arr[i][j]
+                diagonal2 *= arr[i][j]
+            } else if (i == j) {
+                diagonal1 *= arr[i][j]
+            } else if (j + i == arr.size - 1) {
+                diagonal2 *= arr[i][j]
+            }
+        }
+    }
+    println("left side: $diagonal1")
+    println("right side: $diagonal2")
+    println("SumDiagonal: ${diagonal1 + diagonal2}")
+}
+
+fun main(args: Array<String>) {
+
+    val arr = listOf(listOf(2, 2, 3), listOf(3, 4, 5), listOf(3, 4, 5))
+    val arr2 = listOf(listOf(2, 2), listOf(3, 4))
+    println("Appearance Array1: ")
+    for (i in arr) {
+        println(i)
+    }
+    multiDiagonal(arr)
+    println("Appearance Array2: ")
+    for (i in arr) {
+        println(i)
+    }
+    multiDiagonal(arr2)
+
+}
